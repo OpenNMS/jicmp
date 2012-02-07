1a193ab9af4f34d01e3c040efc28bc56a1afedb9
Matt Brozowski <brozow@opennms.org>
2012-02-07 12:58:33 -0500

    add windows commands to build.pl

---
+++ build.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+
+print "Building Java Code\n";
+
+mkdir("classes");
+handle_errors_and_exit_on_failure(system("c:\\Program Files\\Java\\jdk1.6.0_30\\bin\\javac.exe", "-d", "classes", "-sourcepath", "src/main/java", "src/main/java/org/opennms/protocols/icmp/IcmpSocket.java"));
+
+print "Generating JNI Headers\n";
+handle_errors_and_exit_on_failure(system("c:\\Program Files\\Java\\jdk1.6.0_30\\bin\\javah","-classpath", "classes", "org.opennms.protocols.icmp.IcmpSocket")); 
+
+print "Building x86 MSM Modules\n";
+handle_errors_and_exit_on_failure(system("c:\\Program Files\\Microsoft Visual Studio 10.0\\Common7\\IDE\\devenv", "win32\\jicmp.sln", "/build", "Release|Win32"));
+
+
+print "Building x64 MSM Modules\n";
+handle_errors_and_exit_on_failure(system("c:\\Program Files\\Microsoft Visual Studio 10.0\\Common7\\IDE\\devenv", "win32\\jicmp.sln", "/build", "Release|x64")); 
+
+
+sub handle_errors {
+        my $exit = shift;
+       if ($exit == 0) {
+               info("finished successfully");
+        } elsif ($exit == -1) {
+              error("failed to execute: $!");
+        } elsif ($exit & 127) {
+             error("child died with signal " . ($exit & 127));
+        } else {
+             error("child exited with value " . ($exit >> 8));
+        }
+       return $exit;
+}
+
+															
+sub handle_errors_and_exit_on_failure {
+     my $exit = handle_errors(@_);
+    if ($exit != 0) {
+       exit ($exit >> 8);
+    }
+																				}
+
+sub error {
+        print "[ERROR] " . join(' ', @_) . "\n";
+}
+
+sub info {
+        print "[INFO] " . join(' ', @_) . "\n";
+}
